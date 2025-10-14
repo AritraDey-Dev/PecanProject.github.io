f110d3e20585cf33374bde1dd6b4da16b9803707
Aritra Dey <155592377+AritraDey-Dev@users.noreply.github.com>
2025-10-14 10:37:16 +0530

    Revert "Rename 'Twitter' label to '𝕏'"

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -85,9 +85,9 @@ module.exports = {
               className: "footer-slack-link",
             },
             {
-              label: "𝕏 (formerly Twitter)",
-              href: "https://x.com/pecanproject",
-              className: "footer-x-link",
+              label: "Twitter",
+              href: "https://twitter.com/pecanproject",
+              className: "footer-twitter-link",
             },
           ],
         },
