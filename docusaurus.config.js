dce3cec5d8e4604c6eff20f36c2c0cac87448fb4
Aritra Dey <adey01027@gmail.com>
2026-01-31 19:42:06 +0530

    fix: use href for external package documentation and tutorials

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -26,7 +26,7 @@ module.exports = {
               to: "/documentation/", label: "Documentation",
             },
             {
-              to: "/package-documentation/develop/", label: "Package Documentation",
+              href: "/package-documentation/develop/", label: "Package Documentation",
             },
             {
               to: "/tutorials/", label: "Tutorials",
